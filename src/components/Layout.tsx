0d562c3149bbdce87a4130a57a97808b4dd29a32
Raymond Li <liraymond4@gmail.com>
2023-01-28 02:41:54 +1100

    Themes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,61 +12,63 @@ export default function Layout({
     home?: boolean 
   }) {
   return (
-    <div className="flex flex-col items-center">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Raymond Li's Personal Website"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      
-      <header className="w-screen mb-5 flex flex-col items-center">
-        <nav className="w-2/5 my-2 flex flex-row justify-between">
-          <div className="p-2 mx-1 rounded-lg hover:text-black hover:bg-[#f7f7f7] text-xl font-bold">
-            <Link href="/">{siteTitle}</Link>
-          </div>
+    <div className='theme-midnight'>
+      <div className="h-screen flex flex-col items-center bg-skin-fill text-skin-primary">
+            <Head>
+              <link rel="icon" href="/favicon.ico" />
+              <meta
+                name="description"
+                content="Raymond Li's Personal Website"
+              />
+              <meta
+                property="og:image"
+                content={`https://og-image.vercel.app/${encodeURI(
+                  siteTitle,
+                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+              />
+              <meta name="og:title" content={siteTitle} />
+              <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            
+            <header className="w-screen mb-5 flex flex-col items-center">
+              <nav className="w-2/5 my-2 flex flex-row justify-between">
+                <div className="p-2 mx-1 rounded-lg hover:bg-skin-muted text-xl font-bold">
+                  <Link href="/">{siteTitle}</Link>
+                </div>
 
-          <div className="w-1/3 flex justify-between margin-0">
-            <Link href="/projects"><a className="p-2 mx-1 rounded-lg hover:text-black hover:bg-[#f7f7f7]">Projects</a></Link>
-            <Link href="/blog"><a className="p-2 mx-1 rounded-lg hover:text-black hover:bg-[#f7f7f7]">Blog</a></Link>
-            <Link href="/"><a className="p-2 mx-1 rounded-lg hover:text-black hover:bg-[#f7f7f7]">Contact</a></Link>
-          </div>
-        </nav>
-        
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className="rounded-full"
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className="mt-5 text-5xl font-bold tracking-tight">{name}</h1>
-          </>
-        ) : (
-          <></>
-        )}
-      </header>
+                <div className="w-1/3 flex justify-between margin-0">
+                  <Link href="/projects"><a className="p-2 mx-1 rounded-lg hover:bg-skin-muted">Projects</a></Link>
+                  <Link href="/blog"><a className="p-2 mx-1 rounded-lg hover:bg-skin-muted">Blog</a></Link>
+                  <Link href="/"><a className="p-2 mx-1 rounded-lg hover:bg-skin-muted">Contact</a></Link>
+                </div>
+              </nav>
+              
+              {home ? (
+                <>
+                  <Image
+                    priority
+                    src="/images/profile.jpg"
+                    className="rounded-full"
+                    height={144}
+                    width={144}
+                    alt=""
+                  />
+                  <h1 className="mt-5 text-5xl font-bold tracking-tight">{name}</h1>
+                </>
+              ) : (
+                <></>
+              )}
+            </header>
 
-      <div className="w-2/5">
-        <main >{children}</main>
-        {!home && (
-          <div className="mt-12 hover:underline">
-            <Link href="/">← Back to home</Link>
+            <div className="w-2/5">
+              <main >{children}</main>
+              {!home && (
+                <div className="mt-12 hover:underline">
+                  <Link href="/">← Back to home</Link>
+                </div>
+              )}
+            </div>
           </div>
-        )}
-      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,14 @@ module.exports = {
   content: ['./src/**/*.{tsx,ts}'],
   theme: {
     extend: {
-      // colors: {
-      //   'grey': '#f7f7f7'
-      // }
+      colors: {
+        skin: {
+          fill: 'var(--color-bg-fill)',
+          primary: 'var(--color-text-primary)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)',
+        },
+      }
     },
   },
   plugins: [],
