02fae2f3a2390a49bd9670d4d5ca56427741658e
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-02-02 14:50:14 +1100

    Updated themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
           fill: 'var(--color-bg-fill)',
           fillDark: 'var(--color-bg-fill-muted)',
           primary: 'var(--color-text-primary)',
+          accent: 'var(--color-text-accent)',
           muted: 'var(--color-text-muted)',
           inverted: 'var(--color-text-inverted)',
         },
