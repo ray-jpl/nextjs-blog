aa4856be8c07b3c2b12d94fbfc512ef1386bcfab
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-10-16 14:35:43 +1100

    Update spacing with heading

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/Layout';
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link';
-import Date from '../components/date';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/Layout';
+import Date from '../components/date';
+import { getSortedPostsData } from '../lib/posts';
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
@@ -30,7 +30,7 @@ export default function Blog({
         </Head>
   
         <section className="pt-1 text-lg">
-          <h2 className="text-3xl font-bold">Blog</h2>
+          <h2 className="text-3xl font-bold mb-2">Blog</h2>
           <ul className="p-0 m-0 list-none">
             {allPostsData.map(({ id, date, title }) => (
               <li className="m-0 mb-5" key={id}>
